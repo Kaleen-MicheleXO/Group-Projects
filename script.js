3b9ad0963980f86a6b264cfb0b686e42ce21d66d
Glitch (glitch-hello-website) <none>
2022-03-11 02:24:02 +0000

    🐸🛫 Checkpoint ./script.js:41312812/401:41730161/90 ./index.html:41298994/24:41300216/17:41312812/495 ./.glitch-assets:41316838/1066

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
+// Note 1
 function playTRex() {
   var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
   audio.play();
 }
 
-function playMusic() {
+// Note 2
+function playPianoNote() {
   var audio = new Audio('https://cdn.glitch.global/9db1163b-024c-40fd-b302-a4340985e8af/notes_A.mp3?v=1646964279079');
   audio.play();
 }
 
+// Note 3
 function playLetsGo() {
   var audio = new Audio('https://cdn.glitch.global/9db1163b-024c-40fd-b302-a4340985e8af/Lets%20Go.m4a?v=1646963856385');
   audio.play();
 }
 
-
-function bruh() {
-  var audio = new Audio('');
+// Note 4
+function playBruh() {
+  var audio = new Audio('https://cdn.glitch.global/9db1163b-024c-40fd-b302-a4340985e8af/movie_1.mp3?v=1646962857615');
   audio.play();
 }
 
-function playLetsGo() {
-  var audio = new Audio('');
+// Note 5
+function playBaddie() {
+  var audio = new Audio('https://cdn.glitch.global/9db1163b-024c-40fd-b302-a4340985e8af/bobs-burgers-supercut-all-of-tinas-moans.mp3?v=1646962641994');
   audio.play();
 }
 
-function playLetsGo() {
-  var audio = new Audio('');
+// Note 6
+function playBoatsAndLogs() {
+  var audio = new Audio('https://cdn.glitch.global/9db1163b-024c-40fd-b302-a4340985e8af/boats%20and%20logs.m4a?v=1646965006611');
   audio.play();
 }
 
-function playLetsGo() {
+// Note 7
+function play() {
   var audio = new Audio('');
   audio.play();
 }
